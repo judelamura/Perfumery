3226344d9bc73cc758dbeb77be58dc3ae08b693c
JULIA DELAMURA BUFFALO <juju.delamurabuffalo@gmail.com>
2026-05-03 15:26:11 -0300

    Enhance create method to save orders to a file
    The create method now saves the order to a file after adding it to the list. A new saveToFile method has been added to handle file writing.

--- OrderService.java
+++ OrderService.java
@@ -2,17 +2,38 @@ package com.perfumery.domain.services;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.io.FileWriter;
+import java.io.IOException;
 
 import com.perfumery.domain.models.OrderModel;
 
 public class OrderService {
+
     private List<OrderModel> orders = new ArrayList<>();
 
     public void create(OrderModel order){
-        orders.add(order);
+        try {
+            orders.add(order);
+            saveToFile(order);
+        } catch (IOException e) {
+            throw new RuntimeException("Erro ao salvar pedido");
+        }
     }
 
-    public List<OrderModel>getAll(){
+    public List<OrderModel> getAll(){
         return orders;
+    }
+
+    private void saveToFile(OrderModel order) throws IOException {
+        FileWriter writer = new FileWriter("data/orders.csv", true);
+
+        writer.write(
+            order.getId() + "," +
+            order.getUserId() + "," +
+            order.getPerfumeId() + "," +
+            order.getQuantity() + "\n"
+        );
+
+        writer.close();
     }
 }
