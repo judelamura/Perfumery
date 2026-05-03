d96fbc05ffb09d51c3a926dcfb48d16f9c0393cd
JULIA DELAMURA BUFFALO <juju.delamurabuffalo@gmail.com>
2026-05-03 14:17:46 -0300

    Create OrderModel.java

---
+++ OrderModel.java
@@ -0,0 +1,38 @@
+package com.perfumery.domain.models;
+
+import java.util.UUID;
+
+public class OrderModel {
+    private String id;
+    private String userId;
+    private String perfumeId;
+    private int quantity;
+    //Outras propriedades que perfume deveria ter...
+
+    public OrderModel(String userId, String perfumeId, int quantity){
+    this.id = UUID.randomUUID().toString();
+    this.userId = userId;
+    this.perfumeId = perfumeId;
+    this.quantity = quantity;
+}
+    public String getId(){
+    return id;
+    }
+    public String getUserId(){
+    return userId;
+    }
+    public String getPerfumeId(){
+    return perfumeId;
+    }
+    public int getQuantity(){
+    return quantity;
+    }
+
+    public void setPerfumeId(String perfumeId){
+        this.perfumeId = perfumeId;
+    }
+
+    public void setQuantity(int quantity){
+        this.quantity = quantity;
+    }
+}
